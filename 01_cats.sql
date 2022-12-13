db30890d5a7cedf544fb15f464c6f2b9709cd9a6
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-13 10:27:13 +0100

    scripts

---
+++ 01_cats.sql
@@ -0,0 +1,18 @@
+-- Anlegen der Tabelle
+
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `age` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Tabellen einfügen
+
+INSERT INTO `mydb`.`cats` (`id`, `name`, `age`) VALUES (default, "Beauty", 4);
+INSERT INTO `mydb`.`cats` (`id`, `name`, `age`) VALUES (default, "Blacky", 2);
+INSERT INTO `mydb`.`cats` (`id`, `name`, `age`) VALUES (default, "Blinky", 5);
+
+-- Tabelle anzeigen lassen
+
+SELECT * FROM mydb.cats;
