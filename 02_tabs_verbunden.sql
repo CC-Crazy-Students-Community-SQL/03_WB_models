ffef551ce5109b00f9a847ce0634a56387bd4fcd
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-13 12:43:10 +0100

    tabs verbunden

---
+++ 02_tabs_verbunden.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `catName` VARCHAR(45) NOT NULL,
+  `catColor` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servantName` VARCHAR(45) NOT NULL,
+  `servantTime` INT NOT NULL,
+  `cats_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC) VISIBLE,
+  CONSTRAINT `fk_servants_cats`
+    FOREIGN KEY (`cats_id`)
+    REFERENCES `mydb`.`cats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
