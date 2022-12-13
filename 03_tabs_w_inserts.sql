102e714ce4531652b157e4712b1d1b0e1f1af332
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-13 12:55:58 +0100

    insert

---
+++ 03_tabs_w_inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mydb`.`cats` (`id`, `catName`, `catColor`) VALUES (default, "Beauty", "black");
+
+INSERT INTO `mydb`.`servants` (`id`, `servantName`, `servantTime`, `cats_id`) VALUES (default, "Mia", 3, 1);
